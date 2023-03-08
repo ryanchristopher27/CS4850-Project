4ec71ecad55f0008bd6278683575c1930f25c409
Ryan Christopher <ryandchristopher27@gmail.com>
2023-03-08 15:17:27 -0600

    Fixed issue

--- server.py
+++ server.py
@@ -30,56 +30,50 @@ def main():
             conn, addr = s.accept()
             with conn:
                 print(f"Connected by {addr}")
-                while True:
-                    data = conn.recv(1024)
-                    dataStr = data.decode()
+                # while True:
+                data = conn.recv(1024)
+                dataStr = data.decode()
 
-                    # Handle Client Input
-                    sData = dataStr.split(" ")
+                # Handle Client Input
+                sData = dataStr.split(" ")
 
-                    # User Log In
-                    if sData[0] == "login":
-                        userID = sData[1]
-                        userPass = sData[2]
-                        validLogin = validLoginInfo(users, userID, userPass)
-                        if validLogin: # User logged in with correct userID and Password
-                            print(userID + " login.")
-                            conn.sendall(bytes("login confirmed", 'utf-8'))
-                            currentUser = userID
-                        else:
-                            conn.sendall(bytes("Denied. Username or Password incorrect.", 'utf-8'))
-                    
-                    # Create New User
-                    elif sData[0] == "newuser":
-                        userID = sData[1]
-                        userPass = sData[2]
-                        validUser = validNewUser(users, userID)
-                        if validUser: # UserID is not already used
-                            users.append(userID, userPass)
-                            print("New user account created.")
-                            conn.sendall(bytes("New user account created. Please login.", 'utf-8'))
-                        else:
-                            conn.sendall(bytes("Denied. User account already exists.", 'utf-8'))
+                # User Log In
+                if sData[0] == "login":
+                    userID = sData[1]
+                    userPass = sData[2]
+                    validLogin = validLoginInfo(users, userID, userPass)
+                    if validLogin: # User logged in with correct userID and Password
+                        print(userID + " login.")
+                        conn.sendall(bytes("login confirmed", 'utf-8'))
+                        currentUser = userID
+                    else:
+                        conn.sendall(bytes("Denied. Username or Password incorrect.", 'utf-8'))
+                
+                # Create New User
+                elif sData[0] == "newuser":
+                    userID = sData[1]
+                    userPass = sData[2]
+                    validUser = validNewUser(users, userID)
+                    if validUser: # UserID is not already used
+                        users.append(userID, userPass)
+                        print("New user account created.")
+                        conn.sendall(bytes("New user account created. Please login.", 'utf-8'))
+                    else:
+                        conn.sendall(bytes("Denied. User account already exists.", 'utf-8'))
 
-                    # Send Message
-                    elif sData[0] == "send":
-                        message = currentUser + ": " + sData[1]
-                        print(message)
-                        conn.sendall(bytes(message, 'utf-8]')) 
+                # Send Message
+                elif sData[0] == "send":
+                    message = currentUser + ": " + dataStr.strip("send ")
+                    print(message)
+                    conn.sendall(bytes(message, 'utf-8]')) 
 
-                    # Log Out
-                    elif sData[0] == "logout":
-                        serverOpen = False 
-                        print(currentUser + " logout.") 
-                        conn.sendall(bytes(currentUser + " left.", 'utf-8')) 
-                        break       
+                # Log Out
+                elif sData[0] == "logout":
+                    serverOpen = False 
+                    print(currentUser + " logout.") 
+                    conn.sendall(bytes(currentUser + " left.", 'utf-8')) 
+                    break       
 
-                    # print(data)
-                    # if not data:
-                    #     break
-                    # conn.sendall(data)
-                    # if data == "logout":
-                    #     serverOpen = False
 
 # Check if login request was done with correct id and password
 def validLoginInfo(users, userID, userPass):
