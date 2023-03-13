e263f0aec474135bc00484b8366f8700ecc0f80d
Ryan Christopher <ryandchristopher27@gmail.com>
2023-03-13 12:11:32 -0500

    work on v2

--- client.py
+++ client.py
@@ -4,12 +4,15 @@
 
 import socket
 
+# Global Variables
+clientOpen = True
+loggedIn = False
+global userID
+
 def main():
     HOST = "127.0.0.1"
     PORT = 19786  # 1, Last 4 digits of pawprint
 
-    clientOpen = True
-    loggedIn = False
     while clientOpen:
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 
@@ -22,25 +25,12 @@ def main():
             if loggedIn == False:
                 # Log In
                 if inpSplit[0] == "login":
-                    s.sendall(bytes(inp, 'utf-8'))
-                    data = s.recv(1024)
-                    dataStr = data.decode()
-                    print(">" + dataStr)
-                    # Check if login was successful and set loggedIn flag
-                    if dataStr == "login confirmed":
-                        loggedIn = True
+                    userID, loggedIn = login(s, inp, inpSplit)
 
 
                 # Create new user
                 elif inpSplit[0] == "newuser":
-                    # Check length of User ID
-                    if len(inpSplit[1]) > 2 and len(inpSplit[1]) < 33:
-                        # Check length of User Password
-                        if len(inpSplit[2]) > 3 and len(inpSplit[2]) < 9:
-                            s.sendall(bytes(inp, 'utf-8'))
-                            data = s.recv(1024)
-                            dataStr = data.decode()
-                            print(">" + dataStr)
+                    newUser(s, inp, inpSplit)
                 
                 # Invalid Input
                 else:
@@ -48,6 +38,18 @@ def main():
             else:
                 # Send Message
                 if inpSplit[0] == "send":
+                    # Send All Message
+                    if inpSplit[1] == "all":
+                        print("Send All")
+                    # Send Message to Specific User
+                    else:
+                        print("Send to specific UserID")
+                        if inpSplit[1] == userID:
+                            s.sendall(bytes(inp, 'utf-8'))
+                            data = s.recv(1024)
+                            dataStr = data.decode()
+                            print(">" + dataStr)
+
                     if len(inpSplit[1]) > 0 and len(inpSplit[1]) < 257:
                         s.sendall(bytes(inp, 'utf-8'))
                         data = s.recv(1024)
@@ -66,5 +68,28 @@ def main():
                 # Invalid Input
                 else:
                     print("Error, not a valid input")
+
+def login(socket, inp, inputArgs):
+    socket.sendall(bytes(inp, 'utf-8'))
+    data = socket.recv(1024)
+    dataStr = data.decode()
+    print(">" + dataStr)
+    # Check if login was successful and set loggedIn flag
+    if dataStr == "login confirmed":
+        loggedIn = True
+        userID = inputArgs[1]
+    else:
+        loggedIn = False
+        userID = None
+    return userID, loggedIn
+
+def newUser(socket, inp, inputArgs):
+    if len(inputArgs[1]) > 2 and len(inputArgs[1]) < 33:
+        # Check length of User Password
+        if len(inputArgs[2]) > 3 and len(inputArgs[2]) < 9:
+            socket.sendall(bytes(inp, 'utf-8'))
+            data = socket.recv(1024)
+            dataStr = data.decode()
+            print(">" + dataStr)
 
 main()
--- server.py
+++ server.py
@@ -4,11 +4,23 @@
 
 import socket
 
+from _thread import *
+import threading
+
+# Global Variables
+printLock = threading.Lock()
+users = []
+# serverOpen = True
+loggedInUsers = []
+threadCount = 0
+maxThreadCount = 3
+connns = {}
+
 def main():
     HOST = "127.0.0.1" 
     PORT = 19786  # 1, Last 4 digits of pawprint
 
-    users = []
+    # users = []
 
     f = open('users.txt', 'r')
     for line in f:
@@ -19,63 +31,13 @@ def main():
         users.append((splitLine[0], splitLine[1]))
     f.close()
 
-    serverOpen = True
-    currentUser = None
-
-    while serverOpen:
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.bind((HOST, PORT))
-            s.listen()
-            conn, addr = s.accept()
-            with conn:
-                data = conn.recv(1024)
-                dataStr = data.decode()
-
-                # Handle Client Input
-                sData = dataStr.split(" ")
-
-                # User Log In
-                if sData[0] == "login":
-                    userID = sData[1]
-                    userPass = sData[2]
-                    validLogin = validLoginInfo(users, userID, userPass)
-                    if validLogin: # User logged in with correct userID and Password
-                        print(userID + " login.")
-                        conn.sendall(bytes("login confirmed", 'utf-8'))
-                        currentUser = userID
-                    else:
-                        conn.sendall(bytes("Denied. Username or Password incorrect.", 'utf-8'))
-                
-                # Create New User
-                elif sData[0] == "newuser":
-                    userID = sData[1]
-                    userPass = sData[2]
-                    validUser = validNewUser(users, userID)
-                    if validUser: # UserID is not already used
-                        users.append((userID, userPass))
-                        print("New user account created.")
-                        conn.sendall(bytes("New user account created. Please login.", 'utf-8'))
-                    else:
-                        conn.sendall(bytes("Denied. User account already exists.", 'utf-8'))
-
-                # Send Message
-                elif sData[0] == "send":
-                    message = currentUser + ": " + dataStr.strip("send ")
-                    print(message)
-                    conn.sendall(bytes(message, 'utf-8]')) 
-
-                # Log Out
-                elif sData[0] == "logout":
-                    serverOpen = False 
-                    print(currentUser + " logout.") 
-                    conn.sendall(bytes(currentUser + " left.", 'utf-8')) 
-                    f2 = open('users.txt', 'w')
-                    for user in users:
-                        msg = "(" + user[0] + ", " + user[1] + ")\n"
-                        f2.write(msg)
-                    f2.close()
-
-                    break
+    # while serverOpen:
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((HOST, PORT))
+        s.listen()
+        conn, addr = s.accept()
+        printLock.acquire()
+        start_new_thread(threaded, (conn,))
 
 
 # Check if login request was done with correct id and password
@@ -94,5 +56,73 @@ def validNewUser(users, userID):
             valid = False
 
     return valid
+ 
+# thread function
+def threaded(c):
+    currentUser = None
+    while True:
+        # data = c.recv(1024)
+        data = c.recv(2048)
+        dataStr = data.decode()
+
+        # Handle Client Input
+        sData = dataStr.split(" ")
+
+        # User Log In
+        if sData[0] == "login":
+            print("\ntest1")
+            userID = sData[1]
+            userPass = sData[2]
+            validLogin = validLoginInfo(users, userID, userPass)
+            print("\ntest2")
+            if validLogin: # User logged in with correct userID and Password
+                print(userID + " login.")
+                c.sendall(bytes("login confirmed", 'utf-8'))
+                currentUser = userID
+                loggedInUsers.append(currentUser)
+            else:
+                c.sendall(bytes("Denied. Username or Password incorrect.", 'utf-8'))
+        
+        # Create New User
+        elif sData[0] == "newuser":
+            userID = sData[1]
+            userPass = sData[2]
+            validUser = validNewUser(users, userID)
+            if validUser: # UserID is not already used
+                users.append((userID, userPass))
+                print("New user account created.")
+                c.sendall(bytes("New user account created. Please login.", 'utf-8'))
+            else:
+                c.sendall(bytes("Denied. User account already exists.", 'utf-8'))
+
+        # Send Message
+        elif sData[0] == "send":
+            # Send All
+            if sData[1] == "all":
+                print("Send All")
+            else:
+                message = currentUser + ": " + dataStr.strip("send ")
+                print(message)
+                c.sendall(bytes(message, 'utf-8]')) 
+
+        # Log Out
+        elif sData[0] == "logout":
+            serverOpen = False 
+            print(currentUser + " logout.") 
+            c.sendall(bytes(currentUser + " left.", 'utf-8')) 
+            f2 = open('users.txt', 'w')
+            for user in users:
+                msg = "(" + user[0] + ", " + user[1] + ")\n"
+                f2.write(msg)
+            f2.close()
+            loggedInUsers.remove(currentUser)
+            # if len(loggedInUsers) == 0:
+            #     serverOpen = False
+
+            printLock.release()
+            break
+ 
+    # connection closed
+    c.close()
 
 main()
