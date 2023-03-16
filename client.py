067422f2bb6f81100919a8c89b7adcf21b7c348b
Ryan Christopher <ryandchristopher27@gmail.com>
2023-03-16 12:25:39 -0500

    working on v2

--- client.py
+++ client.py
@@ -3,17 +3,90 @@ import socket
 HOST = '127.0.0.1'
 PORT = 19786
 
-def main():
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_socket.connect((HOST, PORT))
-    print(f"Connected to server at {HOST}:{PORT}")
 
-    while True:
-        recipient = input("Enter recipient ID: ")
-        message = input("Enter a message to send: ")
-        client_socket.send(f"{recipient}:{message}".encode())
-        response = client_socket.recv(1024)
-        print(f"Server response: {response.decode()}")
+def main():
+
+    loggedIn = False
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST, PORT))
+
+        # client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # client_socket.connect((HOST, PORT))
+        print(f"Connected to server at {HOST}:{PORT}")
+
+        while True:
+            inp = input(">")
+            inpSplit = inp.split(" ")
+
+            # Client not logged in
+            if loggedIn == False:
+                # Login Function
+                if inpSplit[0] == "login":
+                    s.sendall(bytes(inp, 'utf-8'))
+                    data = s.recv(1024)
+                    dataStr = data.decode()
+                    print(">" + dataStr)
+
+                    if dataStr == "login confirmed":
+                        loggedIn = True
+                
+                # New User Function
+                elif inpSplit[0] == "newuser":
+                    # Check length of User ID
+                    if len(inpSplit[1]) > 2 and len(inpSplit[1]) < 33:
+                        # Check length of User Password
+                        if len(inpSplit[2]) > 3 and len(inpSplit[2]) < 9:
+                            s.sendall(bytes(inp, 'utf-8'))
+                            data = s.recv(1024)
+                            dataStr = data.decode()
+                            print(">" + dataStr)
+                
+                # Invalid Input
+                else:
+                    print("> Denied. Please log in first.")
+
+            # Client logged in
+            else:
+                # Send Functions
+                if inpSplit[0] == "send":
+                    lenCheck = -1
+                    for val in inpSplit[2:]:
+                        lenCheck += len(val) + 1
+                    if lenCheck > 0 and lenCheck < 257:
+                        s.sendall(bytes(inp, 'utf-8'))
+                        data.recv(1024)
+                        dataStr = data.decode()
+                        print(">" + dataStr)
+
+                # Who Function
+                elif inpSplit[0] == "who":
+                    s.sendall(bytes(inp, 'utf-8'))
+                    data.recv(1024)
+                    dataStr = data.decode()
+                    print(">" + dataStr)
+                
+                # Logout Function
+                elif inpSplit[0] == "logout":
+                    s.sendall(bytes(inp, 'utf-8'))
+                    data = s.recv(1024)
+                    dataStr = data.decode()
+                    print(">" + dataStr)
+
+                # Invalid Input
+                else:
+                    print("> Error, not a valid input>")
+
+
+            # recipient = input("Enter recipient ID: ")
+            # message = input("Enter a message to send: ")
+            # client_socket.send(f"{recipient}:{message}".encode())
+            # response = client_socket.recv(1024)
+            # print(f"Server response: {response.decode()}")
+
+            # Print response
+            
+            
 
 if __name__ == '__main__':
     main()
--- server.py
+++ server.py
@@ -5,6 +5,7 @@ HOST = '127.0.0.1'
 PORT = 19786
 MAX_CLIENTS = 3
 
+users = []
 clients = {}  # dictionary to keep track of connected clients
 
 def handle_client(client_socket, client_id):
@@ -13,6 +14,15 @@ def handle_client(client_socket, client_id):
         data = client_socket.recv(1024)
         if not data:
             break
+        dataStr = data.decode()
+        sData = dataStr.split(" ")
+
+
+        if sData[0] == "who":
+            print(clients.keys())
+
+        
+
         # send message to specific other client
         message = f"Client {client_id}: {data.decode()}"
         for id, socket in clients.items():
@@ -22,27 +32,49 @@ def handle_client(client_socket, client_id):
     del clients[client_id]  # remove client from dictionary when it disconnects
 
 def main():
+    userFileCreated = True
+
+    # Try to open users.txt file
+    try:
+        f = open('users.txt', 'r')
+        for line in f:
+            strip1 = line.strip("(")
+            strip2 = strip1.strip(")\n")
+            splitLine= strip2.split(", ")
+
+            users.append((splitLine[0], splitLine[1]))
+        f.close()
+    except IOError:
+        userFileCreated = False
+
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.bind((HOST, PORT))
     server_socket.listen(MAX_CLIENTS)
     print(f"Server is listening on {HOST}:{PORT}")
 
-    client_id = 1  # start with client ID 1
+    # client_id = 1  # start with client ID 1
 
     while True:
         client_socket, client_address = server_socket.accept()
         print(f"New connection from {client_address}")
 
         # add new client to dictionary
-        clients[client_id] = client_socket
+        clients[client_address[1]] = client_socket
 
-        print("New Client ID: ", client_id)
+        client_id = client_address[1]
+
+        # print("New Client ID: ", client_id)
+        print("New Client Address: ", client_address[1])
 
         # start a new thread to handle the client
         client_thread = threading.Thread(target=handle_client, args=(client_socket, client_id))
         client_thread.start()
 
-        client_id += 1  # increment client ID
+        print(client_socket.getpeername()[1])
+
+        print(clients)
+
+        # client_id += 1  # increment client ID
 
 if __name__ == '__main__':
     main()
